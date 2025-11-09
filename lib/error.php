5da267401ad858a95393ec6953978e81ed657fef
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2025-11-09 13:00:30 +0100

    feat: add error handling utility to display errors

---
+++ lib/error.php
@@ -0,0 +1,5 @@
+<?php
+error_reporting(E_ALL);          // Report all kinds of errors
+ini_set('display_errors', 1);    // Show errors on the page
+ini_set('display_startup_errors', 1); // Show errors during PHP startup
+?>
