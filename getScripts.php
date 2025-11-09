ee4a017ddc14767f350a3862d521c5c95b10c567
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2025-11-09 13:00:02 +0100

    build: add jquery library

--- getScripts.php
+++ getScripts.php
@@ -7,6 +7,7 @@ function getScripts()
             lucide.createIcons();
         </script>
         <script src='https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js'></script>
+        <script src='../assets/js/jquery-3.7.1.min.js'></script>
     ";
 }
 ?>
