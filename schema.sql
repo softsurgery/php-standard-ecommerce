caa40082d4764a808755a36f20c7e8e5835d99aa
softsurgery <houssemeddinechibouni@fsb.ucar.tn>
2025-11-25 13:50:56 -0800

    docs: update schema.sql for quiz module

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 23 nov. 2025 à 13:16
+-- Généré le : mar. 25 nov. 2025 à 22:23
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -20,6 +20,62 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `ecommerce`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product-category`
+--
+
+CREATE TABLE `product-category` (
+  `id` int(11) NOT NULL,
+  `label` varchar(20) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `product-category`
+--
+
+INSERT INTO `product-category` (`id`, `label`, `description`) VALUES
+(6, 'AAAA', 'BBBCDAAAAAA');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `question`
+--
+
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL,
+  `label` varchar(4098) NOT NULL,
+  `type` enum('TEXT','CHECKBOX','RADIO','SWITCH','SLIDER') NOT NULL,
+  `details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`details`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `quiz_question`
+--
+
+CREATE TABLE `quiz_question` (
+  `question_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `ordering` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -49,6 +105,31 @@ INSERT INTO `users` (`id`, `name`, `surname`, `birthdate`, `email`, `password`) 
 --
 
 --
+-- Index pour la table `product-category`
+--
+ALTER TABLE `product-category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `quiz_question`
+--
+ALTER TABLE `quiz_question`
+  ADD KEY `question_id` (`question_id`),
+  ADD KEY `quiz_id` (`quiz_id`);
+
+--
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
@@ -57,6 +138,24 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `product-category`
+--
+ALTER TABLE `product-category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `question`
+--
+ALTER TABLE `question`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `users`
