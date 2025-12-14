967bcb6c17aec0c2434361506fa6b12741988cef
softsurgery <houssemeddinechibouni@fsb.ucar.tn>
2025-12-14 14:23:51 +0100

    chore: update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 09 déc. 2025 à 23:17
+-- Généré le : dim. 14 déc. 2025 à 14:23
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -50,24 +50,20 @@ CREATE TABLE `question` (
   `id` int(11) NOT NULL,
   `label` varchar(4098) NOT NULL,
   `type` enum('TEXT','CHECKBOX','RADIO','SWITCH','SLIDER') NOT NULL,
-  `details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`details`))
+  `details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`details`)),
+  `rate` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `question`
 --
 
-INSERT INTO `question` (`id`, `label`, `type`, `details`) VALUES
-(6, 'A repudiandae nobis', 'RADIO', '{\"choices\":[{\"id\":\"Nobis odit in ut con\",\"label\":\"Enim occaecat quis d\",\"correct\":null},{\"id\":\"Dolores aliquam odio\",\"label\":\"Cumque consequatur \",\"correct\":null},{\"id\":\"Totam ex nisi eum qu\",\"label\":\"Quo cupidatat eum vo\",\"correct\":null},{\"id\":\"Praesentium ullamco \",\"label\":\"Rerum qui excepteur \",\"correct\":\"on\"},{\"id\":\"Mollitia est aut com\",\"label\":\"Rerum id laudantium\",\"correct\":\"on\"}],\"min\":\"0\",\"max\":\"100\"}'),
-(7, 'Rerum qui sunt ad a', 'SWITCH', '{\"min\":\"24\",\"max\":\"83\"}'),
-(8, 'Proident odio tempo', 'SWITCH', '{\"min\":\"0\",\"max\":\"100\"}'),
-(9, 'Voluptate reprehende', 'SWITCH', '{\"min\":\"0\",\"max\":\"100\"}'),
-(10, 'Vel nulla dolore sun', 'CHECKBOX', '{\"choices\":[{\"id\":\"Sed ullam sed earum \",\"label\":\"Aliquip id facilis t\",\"correct\":\"on\"},{\"id\":\"Dolores dolores eum \",\"label\":\"Magnam tempore susc\",\"correct\":\"on\"},{\"id\":\"Quos ut nisi tempori\",\"label\":\"Eaque recusandae Ve\",\"correct\":null},{\"id\":\"Quia debitis expedit\",\"label\":\"Commodo deserunt ius\",\"correct\":\"on\"},{\"id\":\"Velit quibusdam cupi\",\"label\":\"Amet quas dicta odi\",\"correct\":null}],\"min\":\"31\",\"max\":\"44\"}'),
-(11, 'Non mollitia ipsum t', 'TEXT', '{\"min\":\"72\",\"max\":\"57\"}'),
-(12, 'Pariatur Enim offic', 'TEXT', '{\"min\":\"21\",\"max\":\"53\"}'),
-(13, 'Repudiandae id repre', 'RADIO', '{\"choices\":[{\"id\":\"Excepteur vel placea\",\"label\":\"Ad ut necessitatibus\",\"correct\":\"on\"},{\"id\":\"Et nisi eveniet eu \",\"label\":\"Libero mollitia ex a\",\"correct\":null}]}'),
-(14, 'Velit velit non aut', 'CHECKBOX', '{\"choices\":[{\"id\":\"Ut est mollit in qui\",\"label\":\"Eveniet quo eveniet\",\"correct\":\"on\"},{\"id\":\"Omnis sit quo molli\",\"label\":\"Perspiciatis eaque \",\"correct\":\"on\"}]}'),
-(15, 'Aut ea voluptas enim', 'SLIDER', '{\"min\":\"0\",\"max\":\"100\"}');
+INSERT INTO `question` (`id`, `label`, `type`, `details`, `rate`) VALUES
+(24, 'Aliqua Reprehenderi', 'SWITCH', '{\"correct\":\"on\"}', 95),
+(25, 'Quia culpa ut incid', 'SWITCH', '{\"correct\":\"off\"}', 58),
+(26, 'Consequuntur quae el', 'TEXT', '{\"correct\":\"Quis expedita obcaec\"}', 26),
+(27, 'Autem veniam soluta', 'RADIO', '{\"choices\":[{\"id\":\"Nihil cumque numquam\",\"label\":\"Pariatur Ad ea itaq\",\"correct\":false},{\"id\":\"Voluptatem animi di\",\"label\":\"Voluptatem iusto ni\",\"correct\":true},{\"id\":\"Amet architecto nec\",\"label\":\"Delectus deserunt a\",\"correct\":false},{\"id\":\"Quos ad esse adipisc\",\"label\":\"Cillum ad non aut qu\",\"correct\":true}]}', 71),
+(28, 'Ex expedita nisi dol', 'RADIO', '{\"choices\":[{\"id\":\"Libero et non aut ip\",\"label\":\"Anim a nostrum quasi\",\"correct\":true},{\"id\":\"Eos fugit beatae f\",\"label\":\"Voluptatem nisi moll\",\"correct\":true},{\"id\":\"Est deserunt incidi\",\"label\":\"Est commodo praesen\",\"correct\":true},{\"id\":\"Sed Nam nisi sint mo\",\"label\":\"Ut voluptas tempore\",\"correct\":false}]}', 70);
 
 -- --------------------------------------------------------
 
@@ -86,8 +82,7 @@ CREATE TABLE `quiz` (
 --
 
 INSERT INTO `quiz` (`id`, `name`, `description`) VALUES
-(3, 'Cooper Norton', 'At do facilis dolore'),
-(4, 'Daria Dickerson', 'Consequuntur quidem');
+(11, 'Brady Gould', 'Autem in ea veniam');
 
 -- --------------------------------------------------------
 
@@ -106,16 +101,11 @@ CREATE TABLE `quiz_question` (
 --
 
 INSERT INTO `quiz_question` (`question_id`, `quiz_id`, `ordering`) VALUES
-(6, 3, 0),
-(7, 3, 1),
-(8, 3, 2),
-(9, 3, 3),
-(10, 3, 4),
-(11, 3, 5),
-(12, 3, 6),
-(13, 4, 0),
-(14, 4, 1),
-(15, 4, 2);
+(24, 11, 0),
+(25, 11, 1),
+(26, 11, 2),
+(27, 11, 3),
+(28, 11, 4);
 
 -- --------------------------------------------------------
 
@@ -129,13 +119,6 @@ CREATE TABLE `submission` (
   `answers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`answers`)),
   `createdAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `submission`
---
-
-INSERT INTO `submission` (`quiz_id`, `user_id`, `answers`, `createdAt`) VALUES
-(3, 3, '{\"6\":\"Mollitia est aut com\",\"7\":\"1\",\"8\":\"1\",\"10\":[\"Sed ullam sed earum \",\"Dolores dolores eum \",\"Velit quibusdam cupi\"],\"11\":\"Sunt adipisci dicta\",\"12\":\"Aliquam laboriosam \"}', '2025-12-09 23:14:58');
 
 -- --------------------------------------------------------
 
@@ -217,13 +200,13 @@ ALTER TABLE `product-category`
 -- AUTO_INCREMENT pour la table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT pour la table `quiz`
 --
 ALTER TABLE `quiz`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `users`
